81bbea418431e5ecfbc10c35d141ebe792b88c86
Hiep VU <hiepv511@gmail.com>
2016-08-01 18:46:07 +0700

    Add style for headline widget and fix some wc products issues

--- includes/widgets/nbtsow-headline-widget/tpl/default.php
+++ includes/widgets/nbtsow-headline-widget/tpl/default.php
@@ -9,12 +9,12 @@
         break;
       case 'first_sub':
         if( !empty($first_sub_text) ) {
-          echo '<' . $first_sub_tag . ' class="nbtsow-headline">' . wp_kses_post( $first_sub_text ) . '</' . $first_sub_tag . '>';
+          echo '<' . $first_sub_tag . ' class="nbtsow-firstsub">' . wp_kses_post( $first_sub_text ) . '</' . $first_sub_tag . '>';
         }
         break;
       case 'second_sub':
         if( !empty($second_sub_text) ) {
-          echo '<' . $second_sub_tag . ' class="nbtsow-headline">' . wp_kses_post( $second_sub_text ) . '</' . $second_sub_tag . '>';
+          echo '<' . $second_sub_tag . ' class="nbtsow-secondsub">' . wp_kses_post( $second_sub_text ) . '</' . $second_sub_tag . '>';
         }
         break;
     }
