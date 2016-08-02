7a8daf3eec3b84b8c8ae0457f7cfcecd75337406
Hiep VU <hiepv511@gmail.com>
2016-08-02 18:33:09 +0700

    Add Members Widget

---
+++ includes/widgets/nbtsow-members-widget/nbtsow-members-widget.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+Widget Name: NetBaseTeam Members Widget
+Description: Widget to beatifully display members with some introduction
+Author: NetBaseTeam
+Author URI: http://netbaseteam.com
+*/
+
+class NBTSOW_Members_Widget extends SiteOrigin_Widget {
+	function __construct() {
+		parent::__construct(
+			'nbtsow-members-widget',
+			esc_html__('NetBaseTeam Members', 'nbtsow'),
+			array(
+				'description' => esc_html__('Widget to display team members', 'nbtsow'),
+			),
+			array(),
+			array(
+				'members' => array(
+					'type' => 'repeater',
+					'label' => esc_html__('Members', 'nbtsow'),
+					'item_name' => esc_html__('Member', 'nbtsow'),
+					'item_label' => array(
+			            'selector'     => "[id*='member_name']",
+			            'update_event' => 'change',
+			            'value_method' => 'val',
+			        ),
+					'fields' => array(
+						'name' => array(
+							'type' => 'text',
+							'label' => esc_html__('Name', 'nbtsow'),
+							'description' => esc_html__('Member\'s name', 'nbtsow')
+						),
+						'image' => array(
+                            'type' => 'media',
+                            'label' => __('Team member image.', 'nbtsow')
+                        ),
+						'profile' => array(
+							'type' => 'textarea',
+							'label' => esc_html__('Introduction', 'nbtsow'),
+							'description' => esc_html__('Short description of this member')
+						),
+						'social' => array(
+							'type' => 'section',
+                            'label' => esc_html__('Social profile', 'nbtsow'),
+							'fields' => array(
+								'facebook' => array(
+									'type' => 'text',
+									'label' => esc_html__('Facebook Page', 'nbtsow'),
+									'description' => esc_html__('Link to the Facebook page of this members', 'nbtsow')
+								),
+								'twitter' => array(
+									'type' => 'text',
+									'label' => esc_html__('Twitter Page', 'nbtsow'),
+									'description' => esc_html__('Link to the Twitter page of this members', 'nbtsow')
+								),
+								'gplus' => array(
+									'type' => 'text',
+									'label' => esc_html__('Google Plus Page', 'nbtsow'),
+									'description' => esc_html__('Link to the Google Plus page of this members', 'nbtsow')
+								)
+							)
+						)
+					)
+				)
+			)
+		);
+	}
+
+	function get_template_variables($instance, $args) {
+		return array(
+			'member' => !empty($instance['members']) ? $instance['members'] : array()
+		);
+	}
+
+	function get_template_name($instance) {
+		return 'default';
+	}
+
+	function get_style_name($instance) {
+		return '';
+	}
+}
+
+siteorigin_widget_register('nbtsow-members-widget', __FILE__, 'NBTSOW_Members_Widget');
---
+++ includes/widgets/nbtsow-members-widget/tpl/default.php
@@ -0,0 +1,45 @@
+<div class="nbtsow-members">
+	<?php foreach($members as $member): ?>
+	<div class="nbtsow-member">
+
+		<?php if($member['image']): ?>
+		<div class="nbtsow-member-thumb">
+			<?php echo wp_get_attachment_image($member['image'], 'full');?>
+			<?php if($member['social']): ?>
+			<div class="nbtsow-member-social">
+
+				<?php if($member['social']['facebook']): ?>
+				<a href="<?php echo esc_url($member['social']['facebook']); ?>">
+					<i class="icon-facebook"></i>
+				</a>
+				<?php endif;?>
+
+				<?php if($member['social']['twitter']): ?>
+				<a href="<?php echo esc_url($member['social']['twitter']); ?>">
+					<i class="icon-twitter"></i>
+				</a>
+				<?php endif;?>
+
+				<?php if($member['social']['gplus']): ?>
+				<a href="<?php echo esc_url($member['social']['gplus']); ?>">
+					<i class="icon-gplus"></i>
+				</a>
+				<?php endif;?>
+
+			</div>
+			<?php endif;?>
+		</div>
+		<?php endif;?>
+
+		<div class="nbtsow-member-details">
+			<?php if($member['name']): ?>
+			<h4 class="nbtsow-member-name"><?php wp_kses_post($member['name']); ?></h4>
+			<?php endif;?>
+			<?php if($member['profile']): ?>
+			<p class="nbtsow-member-description"><?php wp_kses_post($member['profile']); ?></p>
+			<?php endif;?>
+		</div>
+
+	</div>
+	<?php endforeach;?>
+</div>
